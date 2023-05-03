731a893c9f8e2240214bbe750099539004f1b94f
Rilson Almeida <rilson.almeida@gmail.com>
2023-05-02 22:00:03 -0300

    Signupuser Template using UserCreationForm

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html')    
+    form = UserCreationForm()
+    return render(request, 'todo/signupuser.html', {'form': form})    

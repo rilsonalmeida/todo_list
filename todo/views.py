16e7ade684c78566d0b9a5378b50d4d603cbf6ba
Rilson Almeida <rilson.almeida@gmail.com>
2023-05-03 06:35:46 -0300

    Fix message errors in Signupuser

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -6,7 +7,9 @@ from django.contrib.auth.models import User
 def signupuser(request):
     if request.method == 'GET':
         form = UserCreationForm()
-        return render(request, 'todo/signupuser.html', {'form': form})    
+        return render(request, 'todo/signupuser.html', {
+            'form': form,
+            })    
     else:
         # Create a new user, if passwords match
         
@@ -15,8 +18,16 @@ def signupuser(request):
         input_password1 = request.POST['password1']
         input_password2 = request.POST['password2']
         if input_password1 == input_password2:
-            new_user = User.objects.create_user(input_username, password=input_password1)
-            new_user.save()
+            try:
+                new_user = User.objects.create_user(input_username, password=input_password1)
+                new_user.save()
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {
+                            'form': UserCreationForm(),
+                            'error': 'That username has already been taken. Please choose a new one.',
+            })
         else:
-            # message error (to be implemented)
-            pass
+            return render(request, 'todo/signupuser.html', {
+            'form': UserCreationForm(),
+            'error': 'Passwords did not match',
+            })
